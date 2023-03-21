fa3bccbe9692e861e19f6f05f7390a436b7995df
oledibefrancis <somtofrancis380@gmail.com>
2023-03-20 19:42:07 -0500

    Added controller test

--- hello-circle/src/main/java/com/company/hellocircle/controller/HelloCircleController.java
+++ hello-circle/src/main/java/com/company/hellocircle/controller/HelloCircleController.java
@@ -1,13 +1,21 @@
 package com.company.hellocircle.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloCircleController {
 
     @GetMapping(value = "/hello")
+    @ResponseStatus(HttpStatus.OK)
     public String helloCircle(){
         return "Hello, Circle!";
+    }
+
+    @GetMapping(value = "/answer")
+    public String getAnswer(){
+        return "The answer is 42!";
     }
 }
--- hello-circle/src/test/java/com/company/hellocircle/controller/HelloCircleControllerTest.java
+++ hello-circle/src/test/java/com/company/hellocircle/controller/HelloCircleControllerTest.java
@@ -6,12 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.junit.Assert.*;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(HelloCircleController.class)
